3e8126850400f546f45b5daf54b3944316b513ca
PramodSharma122 <pramodchalise122@gmail.com>
2023-08-18 14:11:50 +0545

    first commit

---
+++ scripts/word-list.js
@@ -0,0 +1,23 @@
+const wordList = [
+    {
+        word:"guitar",
+        hint:"A musical instrument with strings."
+    },
+    {
+        word:"oxygen",
+        hint:"A colorless, odorless gas essential for life."
+    },
+    {
+        word:"mountain",
+        hint:"A large natural elevation of the Earth's surface."
+    },
+    {
+        word:"painting",
+        hint:"A art form using colors on a surface to create images or expression."
+    },
+    {
+    word:"drawing",
+    hint:"A scatche in paper."
+    },
+    
+];
