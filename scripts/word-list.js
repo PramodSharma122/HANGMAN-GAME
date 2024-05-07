17d1916aefd04fae0fcfe7d019663b80ab26e991
Pramod Sharma <118656706+PramodSharma122@users.noreply.github.com>
2024-05-07 18:14:56 +0545

    Update word-list.js

--- scripts/word-list.js
+++ scripts/word-list.js
@@ -1,35 +1,75 @@
 const wordList = [
     {
-        word:"guitar",
-        hint:"A musical instrument with strings."
+        word: "guitar",
+        hint: "A musical instrument with strings."
     },
     {
-        word:"oxygen",
-        hint:"A colorless, odorless gas essential for life."
+        word: "oxygen",
+        hint: "A colorless, odorless gas essential for life."
     },
     {
-        word:"mountain",
-        hint:"A large natural elevation of the Earth's surface."
+        word: "mountain",
+        hint: "A large natural elevation of the Earth's surface."
     },
     {
-        word:"painting",
-        hint:"A art form using colors on a surface to create images or expression."
+        word: "painting",
+        hint: "A art form using colors on a surface to create images or expression."
     },
     {
-        word:"drawing",
-        hint:"A scatche in paper."
+        word: "drawing",
+        hint: "A scatche in paper."
     },
     {
-        word:"mars",
-        hint:'Which planet is known as the "Red Planet"?'
+        word: "mars",
+        hint: 'Which planet is known as the "Red Planet"?'
     },
     {
-        word:  "stapes",
-        hint:"What is the smallest bone in the human body?"
+        word: "stapes",
+        hint: "What is the smallest bone in the human body?"
     },
     {
-        hint:"What is the term for a word or phrase that reads the same backward as forward?",
-        word:"Palindrome"
+        hint: "What is the term for a word or phrase that reads the same backward as forward?",
+        word: "palindrome"
+    },
+    {
+        hint: "What is the art of folding paper into decorative shapes and figures called?",
+        word: "origami"
+    },
+    {
+        hint: "What is the study of the Earth's physical structure called?",
+        word: "geology"
+    },
+    {
+        hint: "What is the study of celestial bodies called?",
+        word: "astronomy"
+    },
+    {
+        hint: "What is the largest desert in the world?",
+        word: "sahara"
+    },
+    {
+        hint: "What is the main ingredient in guacamole?",
+        word: "avocado"
+    },
+    {
+        hint: "What is the longest river in the world?",
+        word: "nile"
+    },
+    {
+        hint: "What is the largest organ in the human body?",
+        word: "skin"
+    },
+    {
+        hint:"It can be cracked but its not an egg.",
+        word: "code"
+    },
+    {
+        hint: "Who was the first woman to win a Nobel Prize?",
+        word: "mariecurie"
+    },
+    {
+        hint: "What is the largest mammal in the world?",
+        word: "bluewhale"
     },
 
     
